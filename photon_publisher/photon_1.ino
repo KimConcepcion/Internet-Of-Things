1706e88a2c9b7d306ebeafd8f35f1f45bfeba439
KimConcepcion <kimnielsen33333@gmail.com>
2018-10-10 09:50:50 +0200

    added publish functionality

--- photon_publisher/photon_1.ino
+++ photon_publisher/photon_1.ino
@@ -20,11 +20,16 @@ void loop()
   //  Test input from button:
   if(buttonState == LOW)
   {
+    //  Publish the event:
+    Particle.publish("buttonState", "Pressed", 60, PRIVATE);
 
+    //  Update the buttonstate:
+    buttonState = HIGH;
   }
 
   else
   {
-
+    //  Publish the event:
+    Particle.publish("buttonState", "unPressed", 60, PRIVATE);
   }
 }
