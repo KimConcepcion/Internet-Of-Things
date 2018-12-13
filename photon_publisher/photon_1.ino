6280871e3bdf88b022ccd811df8d175c59deb418
Kim Nielsen <kimnielsen33333@gmail.com>
2018-12-13 20:07:15 +0100

    Update photon_1.ino

--- photon_publisher/photon_1.ino
+++ photon_publisher/photon_1.ino
@@ -3,6 +3,12 @@
 int button = D3;
 
 void setup() {
+
+	//  Subscribe to IFTTT events:
+    //Particle.subscribe("Start_Event", startEvent);
+    //Particle.subscribe("End_Event", endEvent);
+
+    Serial.begin(9600);
     pinMode(button, INPUT);
 }
 
@@ -12,3 +18,25 @@ void loop() {
         delay(500);
     }
 }
+
+/*
+//  Event handler for started events:
+void startEvent(const char *event, const char *data)
+{
+    if( strcmp(event, "Start_Event") == 0 )
+    {
+        Serial.println("Event started!");
+        homeState = false;
+    }
+}
+
+//  Event handler for ended events:
+void endEvent(const char *event, const char *data)
+{
+    if( strcmp(event, "End_Event") == 0 )
+    {
+        Serial.println("Event ended!");
+        homeState = true;
+    }
+}
+*/
